b56f863c4d108a6ecead2f37cf29b64c8507701a
root <root@hello.mobileatsite.com>
2019-11-19 15:53:35 +0530

    First Commit

---
+++ Test11/src/HelloTest11.java
@@ -0,0 +1,12 @@
+
+public class HelloTest11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int first = 10;
+        int second = 20;
+        int sum = first + second;
+        System.out.println("The sum is: " + sum);
+	}
+
+}
